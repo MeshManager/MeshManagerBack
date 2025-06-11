e2d23103d73de7dfb723624de7f18934cc12290e
sujeong11 <qalzm7351@gmail.com>
2025-06-11 14:26:43 +0900

    docs: API 설명 작성

--- agent-service/src/main/java/org/agent/service/api/AgentController.java
+++ agent-service/src/main/java/org/agent/service/api/AgentController.java
@@ -1,5 +1,6 @@
 package org.agent.service.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.agent.service.common.StatusResponse;
 import org.agent.service.dto.RegisterAgentRequest;
@@ -14,11 +15,17 @@ public class AgentController {
 
   private final AgentService agentService;
 
+  @Operation(
+      summary = "고객 k8s에 설치된 agent 정보 등록 API"
+  )
   @PostMapping
   public StatusResponse registerAgent(@RequestBody RegisterAgentRequest request) {
     return agentService.register(request);
   }
 
+  @Operation(
+      summary = "고객 k8s의 클러스터 리소스 상태 확인 API"
+  )
   @PostMapping("/{agentName}/cluster-state")
   public StatusResponse saveClusterState(
       @PathVariable(name = "agentName") String agentName,
@@ -27,6 +34,9 @@ public class AgentController {
     return agentService.saveClusterState(agentName, request);
   }
 
+  @Operation(
+      summary = "고객 k8s에 설치된 agent와의 연결 확인 API"
+  )
   @GetMapping("/{agentName}/status")
   public StatusResponse checkAgentStatus(@PathVariable(name = "agentName") String agentName) {
     return agentService.checkAgentStatus(agentName);
