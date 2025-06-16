575dba046b74cdd271ab53f8cb682961fe10fb19
S6John <john_sungho@naver.com>
2025-06-16 17:30:14 +0900

    init: crd-service

---
+++ crd-service/src/test/java/com/istizo/crd_service/CrdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istizo.crd_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
