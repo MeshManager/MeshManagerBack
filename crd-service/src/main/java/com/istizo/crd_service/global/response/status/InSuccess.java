6585f431a7ae92816f8e91cff1be328b2af4dbb3
S6John <john_sungho@naver.com>
2025-06-16 17:47:35 +0900

    feat: add resposne format

---
+++ crd-service/src/main/java/com/istizo/crd_service/global/response/status/InSuccess.java
@@ -0,0 +1,18 @@
+package com.istizo.crd_service.global.response.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum InSuccess {
+
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
