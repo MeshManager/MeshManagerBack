4aabd605d9410dae738ca94e2cd534e2404dd697
S6John <john_sungho@naver.com>
2025-06-17 16:09:05 +0900

    feat: add service impl & service

--- crd-service/src/main/java/com/istizo/crd_service/service/CrdService/CrdService.java
+++ crd-service/src/main/java/com/istizo/crd_service/service/CrdService/CrdService.java
@@ -1,5 +1,25 @@
 package com.istizo.crd_service.service.CrdService;
 
+import com.istizo.crd_service.web.dto.CrdRequestDTO;
+import com.istizo.crd_service.web.dto.CrdResponseDTO;
+
+import java.util.UUID;
+
 public interface CrdService {
+    CrdResponseDTO.toGetServiceEntityListDTO GetServiceEntityList(UUID uuid);
+    CrdResponseDTO.toGetServiceEntityDTO GetServiceEntityDTO(Long serviceEntityID);
+    CrdResponseDTO.toGetDependencyDTO GetDependencyDTO(Long dependencyID);
+    CrdResponseDTO.toGetdarknessReleaseDTO GetDarknessReleaseDTO(Long darknessReleaseID);
+    CrdResponseDTO.toResponseDependantID GetDependantID(Long serviceEntityID);
+
+    CrdResponseDTO.toResponseID CreateServiceEntity(CrdRequestDTO.toCreateServiceEntityDTO serviceEntityDTO);
+    CrdResponseDTO.toResponseID CreateDependency(CrdRequestDTO.toCreateDependencyDTO dependencyDTO);
+    CrdResponseDTO.toResponseID CreateDarknessRelease(CrdRequestDTO.toCreateDarknessReleaseDTO DarknessReleaseDTO);
+
+    CrdResponseDTO.toResponseID DeleteServiceEntity();
+    CrdResponseDTO.toResponseID DeleteDependency();
+    CrdResponseDTO.toResponseID DeleteDarknessRelease();
+
+
 
 }
--- crd-service/src/main/java/com/istizo/crd_service/service/CrdService/CrdServiceImpl.java
+++ crd-service/src/main/java/com/istizo/crd_service/service/CrdService/CrdServiceImpl.java
@@ -1,5 +1,76 @@
 package com.istizo.crd_service.service.CrdService;
 
-public class CrdServiceImpl implements CrdService {
+import com.istizo.crd_service.repository.DarknessReleaseRepository;
+import com.istizo.crd_service.repository.DependencyRepository;
+import com.istizo.crd_service.repository.ServiceEntityRepository;
+import com.istizo.crd_service.web.dto.CrdRequestDTO;
+import com.istizo.crd_service.web.dto.CrdResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CrdServiceImpl implements CrdService {
+    private final DarknessReleaseRepository darknessReleaseRepository;
+    private final DependencyRepository dependencyRepository;
+    private final ServiceEntityRepository serviceEntityRepository;
+
+    @Override
+    public CrdResponseDTO.toGetServiceEntityListDTO GetServiceEntityList(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toGetServiceEntityDTO GetServiceEntityDTO(Long serviceEntityID) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toGetDependencyDTO GetDependencyDTO(Long dependencyID) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toGetdarknessReleaseDTO GetDarknessReleaseDTO(Long darknessReleaseID) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseDependantID GetDependantID(Long serviceEntityID) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID CreateServiceEntity(CrdRequestDTO.toCreateServiceEntityDTO serviceEntityDTO) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID CreateDependency(CrdRequestDTO.toCreateDependencyDTO dependencyDTO) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID CreateDarknessRelease(CrdRequestDTO.toCreateDarknessReleaseDTO DarknessReleaseDTO) {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID DeleteServiceEntity() {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID DeleteDependency() {
+        return null;
+    }
+
+    @Override
+    public CrdResponseDTO.toResponseID DeleteDarknessRelease() {
+        return null;
+    }
 }
