8c104afe515678be4c4b4d2249b3768793db6065
sujeong11 <qalzm7351@gmail.com>
2025-06-30 15:44:08 +0900

    feat: 어플리케이션 로그 카운터

--- crd-service/src/main/java/com/istizo/crd_service/global/exception/GlobalExceptionHandler.java
+++ crd-service/src/main/java/com/istizo/crd_service/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.istizo.crd_service.global.exception;
 
 import com.istizo.crd_service.global.response.ResForm;
 import com.istizo.crd_service.global.response.status.ErrorStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ResForm<Void>> handleCustomException(CustomException ex) {
+        log.error("Exception occurred", ex);
+
         ErrorStatus errorStatus = ex.getErrorStatus();
         ResForm<Void> response = ResForm.error(
                 errorStatus.getCode(),
@@ -28,6 +32,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.error("Exception occurred", ex);
+
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
