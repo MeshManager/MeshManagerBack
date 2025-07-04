b5e89a78dd339411b90b8c8fefc5ee9d7ed1ff16
Sungho, Kim <john_sungho@naver.com>
2025-07-05 04:51:44 +0900

    feat: ALB용 health check path

---
+++ cluster-management-service/src/main/java/org/management/service/api/healthController.java
@@ -0,0 +1,15 @@
+package org.management.service.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
+public class healthController {
+    @GetMapping(value = "/health", produces = "application/json")
+    public String getALBHealth() {
+        return "Server Status OK";
+    }
+}
