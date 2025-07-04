13067396aa5fa8608d2d48b5f0eb87c54f8c6ed6
Sungho, Kim <john_sungho@naver.com>
2025-07-05 04:48:40 +0900

    faet: health check 경로 통일

---
+++ crd-service/src/main/java/com/istizo/crd_service/web/controller/healthController.java
@@ -0,0 +1,16 @@
+package com.istizo.crd_service.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
+public class healthController {
+    @GetMapping(value = "/health", produces = "application/json")
+    public String getALBHealth() {
+        return "Server Status OK";
+    }
+}
+
